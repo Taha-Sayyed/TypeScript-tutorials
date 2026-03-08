fa9af3295a9f10e8f37f4ae405fe3511491030ff
Taha Sayyed <codetaha786@gmail.com>
2026-03-08 15:32:35 +0530

    feat: add initial TypeScript greet function example and compiler setup comments

---
+++ Setting_up_typescript/src/index.ts
@@ -0,0 +1,9 @@
+// Command: npx tsc --init
+//Command: npx tsc
+
+function greet(person:string):string{
+    return `Hello ${person}, Welcome to Chaicode`
+}
+
+const username:string="Taha Sayyed";
+console.log(greet(username));
