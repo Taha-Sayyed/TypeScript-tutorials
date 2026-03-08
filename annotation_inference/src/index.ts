be60832d3d4e26a9bb2eb18d04cc93f14749d9e2
Taha Sayyed <codetaha786@gmail.com>
2026-03-08 16:43:49 +0530

    feat: add TypeScript example demonstrating type annotation and type inference

---
+++ annotation_inference/src/index.ts
@@ -0,0 +1,8 @@
+let drink="chai";
+// drink=2; //Give error since it has infered that "drink" is a string
+
+let cups=Math.random()>0 ?10:5;
+// cups="taha"; //Give error since it has infered that "cups" is a number
+
+let teaflavor:string="Masala Chai";
+teaflavor="Ginger Tea";
