6517043b13846d40d9944dc183d1a30927e85146
Taha Sayyed <codetaha786@gmail.com>
2026-03-08 15:33:13 +0530

    chore: add compiled JavaScript output for greet example

---
+++ Setting_up_typescript/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Command: npx tsc --init
+//Command: npx tsc
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}, Welcome to Chaicode`;
+}
+const username = "Taha Sayyed";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
