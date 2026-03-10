f227da469802f28b939171d778b4274dadc181a7
Taha Sayyed <codetaha786@gmail.com>
2026-03-10 14:27:08 +0530

    feat: add TypeScript function examples with parameter and return type annotations

---
+++ Function/src/index.ts
@@ -0,0 +1,19 @@
+//Example-1
+function makeChai(type:string,cups:number){
+    console.log(`Making ${cups} cups of ${type}`);
+}
+
+makeChai("Masala",2);
+//------------------------------------------------------
+//Example-2
+function getChaiPrice():number{
+    return 3;
+    // return "3 rupee" //Error
+}
+
+//------------------------------------------------------
+//Example-3
+function log():void{
+    console.log("tea is Ready!");
+}
+
